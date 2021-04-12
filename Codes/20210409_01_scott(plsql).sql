6e08954bd4f8fe41897e27e7edaa98259e857816
Harim Lee <devharimm@gmail.com>
2021-04-12 10:03:15 +0900

    Exception handling and procedure practice

---
+++ Codes/20210409_01_scott(plsql).sql
@@ -0,0 +1,465 @@
+SELECT USER
+FROM DUAL;
+--==>> SCOTT
+
+--○ TBL_STUDENTS 테이블에서
+--   전화번호와 주소 데이터를 수정하는(변경하는) 프로시저를 작성한다.
+--   단, ID 와 PW 가 일치하는 경우에만 수정을 진행할 수 있도록 한다.
+--   프로시저명 : PRC_STUDENTS_UPDATE
+/*
+실행 예)
+EXEC PRC_STUDENTS_UPDATE('superman', 'java006$', '010-9999-9999', '경기 일산');
+
+프로시저 호출로 처리된 결과)
+superman	박정준	010-9999-9999	경기 일산
+*/
+
+-- 함께 풀이한 결과
+CREATE OR REPLACE PROCEDURE PRC_STUDENTS_UPDATE
+( V_ID      IN TBL_IDPW.ID%TYPE
+, V_PW      IN TBL_IDPW.PW%TYPE
+, V_TEL     IN TBL_STUDENTS.TEL%TYPE
+, V_ADDR    IN TBL_STUDENTS.ADDR%TYPE
+)
+IS
+BEGIN
+    -- UPDATE 쿼리문 구성
+    /*
+    UPDATE (SELECT I.ID, I.PW, S.TEL, S.ADDR
+            FROM TBL_IDPW I JOIN TBL_STUDENTS S
+            ON I.ID=S.ID) T
+    SET T.TEL = 입력받은전화번호, T.ADDR = 입력받은주소
+    WHERE T.ID = 입력받은아이디 AND T.PW = 입력받은패스워드;
+    */
+    
+    UPDATE (SELECT I.ID, I.PW, S.TEL, S.ADDR
+            FROM TBL_IDPW I JOIN TBL_STUDENTS S
+            ON I.ID=S.ID) T
+    SET T.TEL = V_TEL, T.ADDR = V_ADDR
+    WHERE T.ID = V_ID AND T.PW = V_PW;
+    
+    -- 커밋
+    COMMIT;
+END;
+--==>> Procedure PRC_STUDENTS_UPDATE이(가) 컴파일되었습니다.
+
+
+--○ TBL_INSA 테이블을 대상으로 신규 데이터 입력 프로시저를 작성한다.
+--   NUM, NAME, SSN, IBSADATE, CITY, TEL, BUSEO, JIKWI, BASICPAY, SUDANG
+--   구조를 갖고 있는 대상 테이블에 데이터 입력 시
+--   NUM 항목(사원번호)의 값은
+--   기존 부여된 사원번호 마지막 번호의 그 다음 번호를
+--   자동으로 입력 처리할 수 있는 프로시저로 구성한다.
+--   프로시저명 : PRC_INSA_INSERT(NAME, SSN, IBSADATE, CITY, TEL, BUSEO, JIKWI, BASICPAY, SUDANG)
+/*
+EXEC PRC_INSA_INSERT('한혜림', '971006-2234567', SYSDATE, '서울', '010-5555-5555'
+                    , '영업부', '대리', 5000000, 500000)
+                    
+프로시저 호출로 처리된 결과)
+1061  한혜림  971006-2234567  2021-04-09  서울  010-5555-5555 영업부  대리  5000000  500000
+*/
+-- 내가 풀이한 내용
+CREATE OR REPLACE PROCEDURE PRC_INSA_INSERT
+( V_NAME        IN TBL_INSA.NAME%TYPE
+, V_SSN         IN TBL_INSA.SSN%TYPE
+, V_IBSADATE    IN TBL_INSA.IBSADATE%TYPE
+, V_CITY        IN TBL_INSA.CITY%TYPE
+, V_TEL         IN TBL_INSA.TEL%TYPE
+, V_BUSEO       IN TBL_INSA.BUSEO%TYPE
+, V_JIKWI       IN TBL_INSA.JIKWI%TYPE
+, V_BASICPAY    IN TBL_INSA.BASICPAY%TYPE
+, V_SUDANG      IN TBL_INSA.SUDANG%TYPE
+)
+IS
+    V_NUM       TBL_INSA.NUM%TYPE;
+    N           NUMBER;
+BEGIN
+    -- 방법1
+    SELECT NUM + 1 INTO V_NUM
+    FROM TBL_INSA
+    WHERE ROWNUM = 1
+    ORDER BY NUM DESC;
+
+    -- 방법2
+    /*
+    SELECT MAX(NUM)+1 INTO V_NUM
+    FROM TBL_INSA;
+    */
+
+    INSERT INTO TBL_INSA(NUM, NAME, SSN, IBSADATE, CITY, TEL, BUSEO, JIKWI, BASICPAY, SUDANG)
+    VALUES (V_NUM, V_NAME, V_SSN, V_IBSADATE, V_CITY, V_TEL, V_BUSEO, V_JIKWI, V_BASICPAY, V_SUDANG);
+END;
+--==>> Procedure PRC_INSA_INSERT이(가) 컴파일되었습니다.
+
+-- 함께 풀이한 내용
+CREATE OR REPLACE PROCEDURE PRC_INSA_INSERT
+( V_NAME        IN TBL_INSA.NAME%TYPE
+, V_SSN         IN TBL_INSA.SSN%TYPE
+, V_IBSADATE    IN TBL_INSA.IBSADATE%TYPE
+, V_CITY        IN TBL_INSA.CITY%TYPE
+, V_TEL         IN TBL_INSA.TEL%TYPE
+, V_BUSEO       IN TBL_INSA.BUSEO%TYPE
+, V_JIKWI       IN TBL_INSA.JIKWI%TYPE
+, V_BASICPAY    IN TBL_INSA.BASICPAY%TYPE
+, V_SUDANG      IN TBL_INSA.SUDANG%TYPE
+)
+IS
+    -- INSERT 쿼리문 수행에 필요한 변수 추가 선언
+    -- → V_NUM
+    V_NUM   TBL_INSA.NUM%TYPE;
+BEGIN
+    SELECT MAX(NUM)+1 INTO V_NUM
+    FROM TBL_INSA;
+    
+    -- INSERT 쿼리문 구성
+    INSERT INTO TBL_INSA(NUM, NAME, SSN, IBSADATE, CITY, TEL, BUSEO, JIKWI, BASICPAY, SUDANG)
+    VALUES (V_NUM, V_NAME, V_SSN, V_IBSADATE, V_CITY, V_TEL, V_BUSEO, V_JIKWI, V_BASICPAY, V_SUDANG);
+END;
+--==>> Procedure PRC_INSA_INSERT이(가) 컴파일되었습니다.
+
+
+
+--○ TBL_상품, TBL_입고 테이블을 대상으로...
+--   TBL_입고 테이블에 데이터 입력 시(즉, 입고 이벤트 발생 시)
+--   TBL_상품 테이블의 재고수량이 함께 변동될 수 있는 기능을 가진
+--   프로시저를 작성한다.
+--   단, 이 과정에서 입고번호는 자동 증가 처리한다. (시퀀스 사용 X)
+--   TBL_입고 테이블 구성 컬럼
+--   → 입고번호, 상품코드, 입고일자, 입고수량, 입고단가
+--   프로시저명 : PRC_입고_INSERT(상품코드, 입고수량, 입고단가)
+CREATE OR REPLACE PROCEDURE PRC_입고_INSERT
+( V_상품코드  IN TBL_입고.상품코드%TYPE
+, V_입고수량  IN TBL_입고.입고수량%TYPE
+, V_입고단가  IN TBL_입고.입고단가%TYPE
+)
+IS
+    V_입고번호  TBL_입고.입고번호%TYPE;
+BEGIN
+    /*
+    SELECT NVL(MAX(입고번호) + 1, 1) INTO V_입고번호
+    FROM TBL_입고;
+    */
+    
+    SELECT COUNT(*)+1 INTO V_입고번호
+    FROM TBL_입고;
+    
+    INSERT INTO TBL_입고(입고번호, 상품코드, 입고일자, 입고수량, 입고단가)
+    VALUES(V_입고번호, V_상품코드, SYSDATE, V_입고수량, V_입고단가);
+    
+    UPDATE TBL_상품
+    SET 재고수량 = 재고수량 + V_입고수량
+    WHERE 상품코드 = V_상품코드;
+    
+    --커밋
+    COMMIT;
+END;
+--==>> Procedure PRC_입고_INSERT이(가) 컴파일되었습니다.
+
+--※ TBL_입고 테이블에 입고 이벤트 발생 시...
+--   관련 테이블에서 수행되어야 하는 내용
+--   ① INSERT → TBL_입고
+--      INSERT INTO TBL_입고(입고번호, 상품코드, 입고일자, 입고수량, 입고단가)
+--      VALUES(1, 'H001', SYSDATE, 20, 900);
+--   ② UPDATE → TBL_상품
+--      UPDATE TBL_상품
+--      SET 재고수량 = 입고수량
+--      WHERE 상품코드 = 'H001';
+
+CREATE OR REPLACE PROCEDURE PRC_입고_INSERT
+( V_상품코드  IN TBL_상품.상품코드%TYPE    -- 상품코드는 두 테이블에 다 있지만 TBL_상품이 부모테이블임 
+, V_입고수량  IN TBL_입고.입고수량%TYPE
+, V_입고단가  IN TBL_입고.입고단가%TYPE
+)
+IS
+    -- 아래의 쿼리문을 수행하기 위해 필요한 데이터를 변수로 선언
+    V_입고번호  TBL_입고.입고번호%TYPE;
+BEGIN
+    -- 선언한 변수(V_입고번호)에 값 담아내기
+    SELECT NVL(MAX(입고번호), 0) INTO V_입고번호
+    FROM TBL_입고;
+    
+    -- 쿼리문 구성
+    -- ① INSERT → TBL_입고
+    INSERT INTO TBL_입고(입고번호, 상품코드, 입고수량, 입고단가) -- 입고일자는 DEFAULT 처리가 되어있어서 안써줘도 됨
+    VALUES((V_입고번호+1), V_상품코드, V_입고수량, V_입고단가);
+    
+    -- ② UPDATE → TBL_상품
+    UPDATE TBL_상품
+    SET 재고수량 = 재고수량 + V_입고수량
+    WHERE 상품코드 = V_상품코드;
+    
+    -- 커밋
+    COMMIT;
+    
+    -- 예외 처리 
+    EXCEPTION
+        WHEN OTHERS THEN ROLLBACK;
+END;
+--==>> Procedure PRC_입고_INSERT이(가) 컴파일되었습니다.
+--> 트랜잭션 처리(은행 예시 기억, 나현이가 가영이한테 100만원을 빌림~!)
+
+
+--■■■ 프로시저 내에서의 예외 처리 ■■■--
+
+--○ 테이블 생성(TBL_MEMBER) → 20210409_02_scott.sql 파일 참조
+
+--○ TBL_MEMBER 테이블에 데이터를 입력하는 프로시저를 생성
+--   단, 이 프로시저를 통해 데이터를 입력할 경우
+--   CITY(지역) 항목에 '서울', '경기', '인천' 만 입력이 가능하도록 구성한다.
+--   이 지역 외의 지역을 프로시저 호출을 통해 입력하려는 경우
+--   예외 처리를 하려고 한다.
+--   프로시저명 : PRC_MEMBER_INSERT(이름, 전화번호, 지역)
+
+CREATE OR REPLACE PROCEDURE PRC_MEMBER_INSERT
+( V_NAME    IN TBL_MEMBER.NAME%TYPE
+, V_TEL     IN TBL_MEMBER.TEL%TYPE
+, V_CITY    IN TBL_MEMBER.CITY%TYPE
+)
+IS -- 오라클의 IS영역은 순차적으로 수행됨
+
+    -- 실행 영역의 쿼리문 수행을 위해 필요한 데이터 변수 선언
+    V_NUM   TBL_MEMBER.NUM%TYPE;
+    
+    -- 사용자 정의 예외에 대한 변수 선언~!!!  CHECK~!!!
+    USER_DEFINE_ERROR EXCEPTION;
+    
+BEGIN -- 오라클의 BEGIN영역은 순차적으로 수행됨
+
+    -- 프로시저를 통해 입력 처리를 정상적으로 진행해야 할 데이터인지
+    -- 아닌지의 여부를 가장 먼저 확인할 수 있도록 코드 구성
+    
+    IF(V_CITY NOT IN ('서울', '경기','인천'))     -- '서울', '경기', '인천' 중 하나가 들어있지 않다면
+        -- 예외 발생~!!! CHECK~!!!
+        THEN RAISE USER_DEFINE_ERROR;            -- 자바의 Throw~ 예외 발생 구문과 같은 기능
+    END IF;                                      -- 예외 발생되면 하던거 멈추고 EXCEPTION 찾아감
+    
+    -- 선언한 변수에 값 담아내기
+    SELECT NVL(MAX(NUM), 0) INTO V_NUM  -- 0 OR 최대값
+    FROM TBL_MEMBER;
+
+    -- 쿼리문 구성(INSERT)
+    INSERT INTO TBL_MEMBER(NUM, NAME, TEL, CITY)
+    VALUES((V_NUM+1), V_NAME, V_TEL, V_CITY);
+    
+    -- 커밋
+    COMMIT;
+    
+    -- 예외 처리
+    /*
+    EXCEPTION
+        WHEN 이런 예외라면
+            THEN 이렇게 처리하고,
+        WHEN 저런 예외라면
+            THEN 저렇게 처리해라
+   
+    
+    EXCEPTION
+        WHEN USER_DEFINE_ERROR
+            THEN RAISE_APPLICATION_ERROR(-에러코드, 에러내용기술);
+            -- 에러코드 20000번까지는 오라클이 선점해서 쓰고 있는 에러코드이기 때문에 
+            -- 20000번 이후부터 자유롭게 사용
+        WHEN 저런 예외라면
+            THEN 저렇게 처리해라 
+    */
+    
+    EXCEPTION
+        WHEN USER_DEFINE_ERROR
+            THEN RAISE_APPLICATION_ERROR(-20001, '서울,인천,경기만 입력 가능합니다.'); 
+            ROLLBACK;
+        WHEN OTHERS
+            THEN ROLLBACK;   
+END;
+--==>> Procedure PRC_MEMBER_INSERT이(가) 컴파일되었습니다.
+
+
+--○ TBL_출고 테이블에 데이터 입력 시(즉, 출고 이벤트 발생 시)
+--   TBL_상품 테이블의 재고수량이 변동되는 프로시저를 작성한다.
+--   단, 출고번호는 입고번호와 마찬가지로 자동 증가.
+--   또한, 출고 수량이 재고 수량보다 많은 경우...
+--   출고 액션을 취소할 수 있도록 처리한다. (출고가 이루어지지 않도록)
+--   프로시저명 : PRC_출고_INSERT(상품코드, 출고수량, 출고단가)
+
+CREATE OR REPLACE PROCEDURE PRC_출고_INSERT
+( V_상품코드    IN TBL_출고.상품코드%TYPE
+, V_출고수량    IN TBL_출고.출고수량%TYPE
+, V_출고단가    IN TBL_출고.출고단가%TYPE
+)
+IS
+    V_출고번호         TBL_출고.출고번호%TYPE;
+    V_재고수량         TBL_상품.재고수량%TYPE;    
+    USER_DEFINE_ERROR EXCEPTION;
+BEGIN
+    -- 특정 상품의 재고를 V_재고수량 변수에 담기
+    SELECT 재고수량 INTO V_재고수량
+    FROM TBL_상품
+    WHERE 상품코드 = V_상품코드;
+
+    -- 출고수량이 재고수량보다 많은 경우 예외를 발생시키는 구문
+    IF (V_출고수량 > V_재고수량)
+        THEN RAISE USER_DEFINE_ERROR;
+    END IF;
+
+    -- 출고번호 자동 증가 구문
+    SELECT NVL(MAX(출고번호), 0) + 1 INTO V_출고번호
+    FROM TBL_출고;
+    
+    -- 출고 성공 시 TBL_출고 테이블에 데이터 입력
+    INSERT INTO TBL_출고(출고번호, 상품코드, 출고수량, 출고단가) -- 출고일자는 DEFAULT SYSDATE라 생략
+    VALUES (V_출고번호, V_상품코드, V_출고수량, V_출고단가);
+    
+    -- 출고 성공 시 TBL_상품 테이블에서 데이터 수정
+    UPDATE TBL_상품
+    SET 재고수량 = 재고수량 - V_출고수량
+    WHERE 상품코드 = V_상품코드;
+    
+    -- 커밋
+    COMMIT;
+    
+    -- 예외처리
+    EXCEPTION
+        WHEN USER_DEFINE_ERROR
+            THEN RAISE_APPLICATION_ERROR(-20001, '출고수량이 재고수량보다 많습니다.');
+                 ROLLBACK;
+        WHEN OTHERS
+            THEN ROLLBACK;
+END;
+--==>> Procedure PRC_출고_INSERT이(가) 컴파일되었습니다.
+
+
+-- 함께 풀이한 내용
+CREATE OR REPLACE PROCEDURE PRC_출고_INSERT
+( V_상품코드    IN TBL_출고.상품코드%TYPE
+, V_출고수량    IN TBL_출고.출고수량%TYPE
+, V_출고단가    IN TBL_출고.출고단가%TYPE
+)
+IS
+    -- 주요 변수 선언
+    V_출고번호  TBL_출고.출고번호%TYPE;
+    V_재고수량  TBL_상품.재고수량%TYPE;
+    
+    USER_DEFINE_ERROR EXCEPTION;       -- 사용자 정의 예외
+    
+BEGIN
+    -- 쿼리문 수행 이전에 수행 여부 확인 → 기존 재고 확인 → 출고 수량과 비교
+    SELECT 재고수량 INTO V_재고수량
+    FROM TBL_상품
+    WHERE 상품코드 = V_상품코드;
+    
+    -- 출고를 정상적으로 진행해 줄 것인지에 대한 여부 확인
+    -- (파악한 재고수량보다 출고수량이 많으면, 예외발생)
+    IF(V_출고수량 > V_재고수량)
+        THEN RAISE USER_DEFINE_ERROR;
+    END IF;
+    
+    -- 선언한 변수에 값 담아내기
+    SELECT NVL(MAX(출고번호), 0) INTO V_출고번호
+    FROM TBL_출고;
+
+    -- 쿼리문 구성(INSERT → TBL_출고)
+    INSERT INTO TBL_출고(출고번호, 상품코드, 출고수량, 출고단가)
+    VALUES((V_출고번호+1), V_상품코드, V_출고수량, V_출고단가);
+    
+    -- 쿼리문 구성(UPDATE → TBL_상품)
+    UPDATE TBL_상품
+    SET 재고수량 = 재고수량 - V_출고수량
+    WHERE 상품코드 = V_상품코드;
+    
+    -- 커밋
+    COMMIT;
+    
+    -- 예외 처리
+    EXCEPTION
+        WHEN USER_DEFINE_ERROR
+            THEN RAISE_APPLICATION_ERROR(-20002, '재고 부족~!!!');
+                 ROLLBACK;
+        WHEN OTHERS
+            THEN ROLLBACK;
+    
+END;
+--==>> Procedure PRC_출고_INSERT이(가) 컴파일되었습니다.
+
+-- SELECT 재고수량 한 것을 변수에 담아서 변수에 담긴 내용과 비교를 해야함
+-- PL/SQL에서 SELECT 문과 바로 비교할 수는 없음 
+
+
+--○ TBL_출고 테이블에서 출고 수량을 변경(수정)하는 프로시저를 작성한다.
+--   프로시저명 : PRC_출고_UPDATE(출고번호, 변경할 수량);
+CREATE OR REPLACE PROCEDURE PRC_출고_UPDATE
+( V_출고번호    IN TBL_출고.출고번호%TYPE
+, V_변경수량    IN TBL_출고.출고수량%TYPE
+)
+IS
+    V_재고수량  TBL_상품.재고수량%TYPE;
+    V_출고수량  TBL_출고.출고수량%TYPE;
+    
+    USER_DEFINE_ERROR  EXCEPTION;
+BEGIN
+    -- 출고 테이블의 출고수량을 V_출고수량 변수에 담기
+    SELECT 출고수량 INTO V_출고수량
+    FROM TBL_출고
+    WHERE 출고번호 = V_출고번호;
+    
+    -- 상품 테이블의 재고수량을 V_재고수량 변수에 담기
+    SELECT 재고수량 INTO V_재고수량
+    FROM TBL_상품
+    WHERE 상품코드 = ( SELECT 상품코드
+                       FROM TBL_출고
+                       WHERE 출고번호 = V_출고번호 );
+
+    -- 변경수량이 (재고수량 + 출고수량) 보다 크면 출고를 진행할 수 없음 → 예외 발생
+    IF(V_변경수량 > (V_재고수량 + V_출고수량))
+        THEN RAISE USER_DEFINE_ERROR;
+    END IF;
+
+    -- 재고수량 변경하는 UPDATE 쿼리
+    UPDATE TBL_상품
+    SET 재고수량 = (V_재고수량 + V_출고수량) - V_변경수량
+    WHERE 상품코드 = ( SELECT 상품코드
+                       FROM TBL_출고
+                       WHERE 출고번호 = V_출고번호 );
+    
+    -- 출고수량 변경하는 UPDATE 쿼리
+    -- 재고수량 변경시 출고수량을 사용하기 때문에 출고수량을 나중에 업데이트 하도록 구성
+    UPDATE TBL_출고
+    SET 출고수량 = V_변경수량
+    WHERE 출고번호 = V_출고번호;
+    
+    -- 커밋
+    COMMIT;
+    
+    -- 예외 처리
+    EXCEPTION
+        WHEN USER_DEFINE_ERROR
+            THEN RAISE_APPLICATION_ERROR(-20003, '변경할 수량이 재고수량보다 많습니다.');
+                 ROLLBACK;
+        WHEN OTHERS
+            THEN ROLLBACK;   
+END;
+--==>> Procedure PRC_출고_UPDATE이(가) 컴파일되었습니다.
+--> 테스트 할 때도 커밋을 넣고 작성하는게 바람직하다
+--  결과를 확인 후 데이터를 이전상태로 복구해두는게 맞음.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Codes/20210409_02_scott.sql
@@ -0,0 +1,617 @@
+SELECT USER
+FROM DUAL;
+--==>> SCOTT
+
+-- 함께 풀이한 내용 
+UPDATE 테이블명
+SET 수정대상=수정내용 
+WHERE 조건;
+
+SELECT I.ID, I.PW, S.TEL, S.ADDR
+FROM TBL_IDPW I JOIN TBL_STUDENTS S
+ON I.ID=S.ID;
+
+UPDATE (SELECT I.ID, I.PW, S.TEL, S.ADDR
+        FROM TBL_IDPW I JOIN TBL_STUDENTS S
+        ON I.ID=S.ID) T
+SET T.TEL = 입력받은전화번호, T.ADDR = 입력받은주소
+WHERE T.ID = 입력받은 아이디 AND T.PW = 입력받은패스워드;
+
+
+--○ 생성한 프로시저(PRC_STUDENTS_UPDATE)가 제대로 동작하는지의 여부 확인
+--   → 프로시저 호출
+EXEC PRC_STUDENTS_UPDATE('superman', 'net007$', '010-9999-9999', '경기 일산');
+--==>> PL/SQL 프로시저가 성공적으로 완료되었습니다.
+--> ID 와 PW 가 유효하지 않은 데이터로 테스트 한 결과, 데이터 변경 실패
+
+SELECT *
+FROM TBL_STUDENTS;
+--==>>
+/*
+superman	박정준	010-1111-1111	제주도 서귀포시
+happyday	김서현	010-2222-2222	서울 마포구
+*/
+SELECT *
+FROM TBL_IDPW;
+--==>>
+/*
+superman	java006$
+happyday	java006$
+*/
+
+
+--○ 생성한 프로시저(PRC_STUDENTS_UPDATE)가 제대로 동작하는지의 여부 확인
+--   → 프로시저 호출
+EXEC PRC_STUDENTS_UPDATE('superman', 'java006$', '010-9999-9999', '경기 일산');
+--==>> PL/SQL 프로시저가 성공적으로 완료되었습니다.
+--> ID 와 PW 가 모두 유효한 데이터로 테스트 한 결과, 데이터 변경 성공
+
+SELECT *
+FROM TBL_STUDENTS;
+--==>>
+/*
+superman	박정준	010-9999-9999	경기 일산
+happyday	김서현	010-2222-2222	서울 마포구
+*/
+SELECT *
+FROM TBL_IDPW;
+--==>>
+/*
+superman	java006$
+happyday	java006$
+*/
+
+ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-MM-DD';
+--==>> Session이(가) 변경되었습니다.
+
+SELECT *
+FROM TBL_INSA;
+
+
+--○ 생성한 프로시저(PRC_INSA_INSERT)가 제대로 동작하는지의 여부 확인
+--   → 프로시저 호출
+EXEC PRC_INSA_INSERT('한혜림', '971006-2234567', SYSDATE, '서울', '010-5555-5555', '영업부', '대리', 5000000, 500000)
+--==>> PL/SQL 프로시저가 성공적으로 완료되었습니다.
+
+ROLLBACK;
+
+DELETE
+FROM TBL_INSA
+WHERE NUM=1061;
+
+--○ 생성한 프로시저(PRC_INSA_INSERT)가 제대로 동작하는지의 여부 확인
+--   → 프로시저 호출
+EXEC PRC_INSA_INSERT('박민지', '960907-2234567', SYSDATE, '서울', '010-6666-6666', '기획부', '대리', 5000000, 500000)
+--==>> PL/SQL 프로시저가 성공적으로 완료되었습니다.
+
+SELECT *
+FROM TBL_INSA
+ORDER BY 1;
+
+--------------------------------------------------------------------------------
+
+--○ 실습 테이블 생성(TBL_상품)
+CREATE TABLE TBL_상품
+( 상품코드      VARCHAR2(20)
+, 상품명        VARCHAR2(100)
+, 소비자가격    NUMBER
+, 재고수량      NUMBER  DEFAULT 0
+, CONSTRAINT 상품_상품코드_PK PRIMARY KEY(상품코드)
+);
+--==>> Table TBL_상품이(가) 생성되었습니다.
+--> 실습을 위해 한글로 된 컬럼을 생성했을 뿐,
+--  실무에서는 한글 테이블, 한글 컬럼 사용하면 안됨.
+
+
+--○ 실습 테이블 생성(TBL_입고)
+CREATE TABLE TBL_입고
+( 입고번호  NUMBER
+, 상품코드  VARCHAR2(20)
+, 입고일자  DATE    DEFAULT SYSDATE
+, 입고수량  NUMBER
+, 입고단가  NUMBER
+, CONSTRAINT 입고_입고번호_PK PRIMARY KEY(입고번호)
+, CONSTRAINT 입고_상품코드_FK FOREIGN KEY(상품코드)
+             REFERENCES TBL_상품(상품코드)
+);
+--==>> Table TBL_입고이(가) 생성되었습니다.
+-- TBL_입고 테이블의 입고번호를 기본키(PK) 제약조건 설정
+-- TBL_입고 테이블의 상품코드는 TBL_상품 테이블의 상품코드를
+-- 참조할 수 있도록 외래키(FK) 제약조건 설정
+
+--○ TBL_상품 테이블에 상품 데이터 입력
+INSERT INTO TBL_상품(상품코드, 상품명, 소비자가격)
+VALUES('H001', '홈런볼', 1500);
+INSERT INTO TBL_상품(상품코드, 상품명, 소비자가격)
+VALUES('H002', '새우깡', 1200);
+INSERT INTO TBL_상품(상품코드, 상품명, 소비자가격)
+VALUES('H003', '자갈치', 1000);
+INSERT INTO TBL_상품(상품코드, 상품명, 소비자가격)
+VALUES('H004', '감자깡', 900);
+INSERT INTO TBL_상품(상품코드, 상품명, 소비자가격)
+VALUES('H005', '꼬깔콘', 1100);
+INSERT INTO TBL_상품(상품코드, 상품명, 소비자가격)
+VALUES('H006', '꼬북칩', 2000);
+INSERT INTO TBL_상품(상품코드, 상품명, 소비자가격)
+VALUES('H007', '맛동산', 1700);
+--==>> 1 행 이(가) 삽입되었습니다. * 7
+
+INSERT INTO TBL_상품(상품코드, 상품명, 소비자가격)
+VALUES('C001', '다이제', 2000);
+INSERT INTO TBL_상품(상품코드, 상품명, 소비자가격)
+VALUES('C002', '사브레', 1800);
+INSERT INTO TBL_상품(상품코드, 상품명, 소비자가격)
+VALUES('C003', '에이스', 1700);
+INSERT INTO TBL_상품(상품코드, 상품명, 소비자가격)
+VALUES('C004', '버터링', 1900);
+INSERT INTO TBL_상품(상품코드, 상품명, 소비자가격)
+VALUES('C005', '아이비', 1700);
+INSERT INTO TBL_상품(상품코드, 상품명, 소비자가격)
+VALUES('C006', '웨하스', 1200);
+INSERT INTO TBL_상품(상품코드, 상품명, 소비자가격)
+VALUES('C007', '오레오', 1900);
+--==>> 1 행 이(가) 삽입되었습니다. * 7
+
+INSERT INTO TBL_상품(상품코드, 상품명, 소비자가격)
+VALUES('E001', '엠엔엠', 600);
+INSERT INTO TBL_상품(상품코드, 상품명, 소비자가격)
+VALUES('E002', '아폴로', 500);
+INSERT INTO TBL_상품(상품코드, 상품명, 소비자가격)
+VALUES('E003', '쫀드기', 300);
+INSERT INTO TBL_상품(상품코드, 상품명, 소비자가격)
+VALUES('E004', '비틀즈', 600);
+INSERT INTO TBL_상품(상품코드, 상품명, 소비자가격)
+VALUES('E005', '마이쮸', 800);
+INSERT INTO TBL_상품(상품코드, 상품명, 소비자가격)
+VALUES('E006', '에그몽', 900);
+INSERT INTO TBL_상품(상품코드, 상품명, 소비자가격)
+VALUES('E007', '차카니', 900);
+--==>> 1 행 이(가) 삽입되었습니다. * 7
+
+SELECT *
+FROM TBL_상품;
+--==>>
+/*
+H001	홈런볼	1500	0
+H002	새우깡	1200	0
+H003	자갈치	1000	0
+H004	감자깡	 900	0
+H005	꼬깔콘	1100	0
+H006	꼬북칩	2000	0
+H007	맛동산	1700	0
+C001	다이제	2000	0
+C002	사브레	1800	0
+C003	에이스	1700	0
+C004	버터링	1900	0
+C005	아이비	1700	0
+C006	웨하스	1200	0
+C007	오레오	1900	0
+E001	엠엔엠	 600	0
+E002	아폴로	 500	0
+E003	쫀드기	 300	0
+E004	비틀즈	 600	0
+E005	마이쮸	 800	0
+E006	에그몽	 900	0
+E007	차카니	 900	0
+*/
+
+SELECT COUNT(*)
+FROM TBL_상품;
+--==>> 21
+
+--○ 커밋
+COMMIT;
+--==>> 커밋 완료.
+
+
+--○ 생성한 프로시저(PRC_INSA_INSERT)가 제대로 동작하는지의 여부 확인
+--   → 프로시저 호출
+
+-- 내가 풀이한 내용
+EXEC PRC_입고_INSERT('H005', 1000, 700);
+EXEC PRC_입고_INSERT('C003', 800, 950);
+EXEC PRC_입고_INSERT('E006', 500, 450);
+EXEC PRC_입고_INSERT('E007', 1000, 550);
+--==>> PL/SQL 프로시저가 성공적으로 완료되었습니다.
+
+SELECT *
+FROM TBL_상품;
+--==>>
+/*
+H001	홈런볼	1500	0
+H002	새우깡	1200	0
+H003	자갈치	1000	0
+H004	감자깡	 900	0
+H005	꼬깔콘	1100	1000
+H006	꼬북칩	2000	0
+H007	맛동산	1700	0
+C001	다이제	2000	0
+C002	사브레	1800	0
+C003	에이스	1700	800
+C004	버터링	1900	0
+C005	아이비	1700	0
+C006	웨하스	1200	0
+C007	오레오	1900	0
+E001	엠엔엠	 600	0
+E002	아폴로	 500	0
+E003	쫀드기	 300	0
+E004	비틀즈	 600	0
+E005	마이쮸	 800	0
+E006	에그몽	 900	500
+E007	차카니	 900	1000
+*/
+SELECT *
+FROM TBL_입고;
+--==>>
+/*
+1	H005	2021-04-09	1000	700
+2	C003	2021-04-09	 800	950
+3	E006	2021-04-09	 500	450
+4	E007	2021-04-09	1000	550
+*/
+
+ROLLBACK;
+--==>> 롤백 완료.
+--> 확인을 위해 COMMIT 작성하지 않았기 때문에 롤백으로 되돌릴 수 있었음.
+--  확인 후 COMMIT 추가함
+
+-- 함께 풀이한 내용
+--   → 프로시저 호출
+EXEC PRC_입고_INSERT('H001', 20, 900);
+--==>> PL/SQL 프로시저가 성공적으로 완료되었습니다.
+
+SELECT *
+FROM TBL_입고;
+--==>> 1	H001	2021-04-09	20	900
+
+SELECT *
+FROM TBL_상품;
+--==>> H001	홈런볼	1500	20
+
+--   → 프로시저 호출
+EXEC PRC_입고_INSERT('H001', 40, 1000);
+--==>> PL/SQL 프로시저가 성공적으로 완료되었습니다.
+
+SELECT *
+FROM TBL_입고;
+--==>>
+/*
+1	H001	2021-04-09	20	900
+2	H001	2021-04-09	40	1000
+*/
+
+SELECT *
+FROM TBL_상품;
+--==>> H001	홈런볼	1500	60
+
+
+--■■■ 프로시저 내에서의 예외 처리 ■■■--
+
+--○ 테이블 생성(TBL_MEMBER)
+CREATE TABLE TBL_MEMBER
+( NUM   NUMBER
+, NAME  VARCHAR2(30)
+, TEL   VARCHAR2(60)
+, CITY  VARCHAR2(60)
+, CONSTRAINT MEMBER_NUM_PK PRIMARY KEY(NUM)
+);
+--==>> Table TBL_MEMBER이(가) 생성되었습니다.
+
+
+--○ 생성한 프로시저(PRC_MEMBER_INSERT)가 제대로 동작하는지의 여부 확인
+--   → 프로시저 호출
+EXEC PRC_MEMBER_INSERT('정주희', '010-1111-1111', '서울');
+--==>> PL/SQL 프로시저가 성공적으로 완료되었습니다.
+
+SELECT *
+FROM TBL_MEMBER;
+--==>> 1	정주희	010-1111-1111	서울
+
+
+
+--○ 생성한 프로시저(PRC_MEMBER_INSERT)가 제대로 동작하는지의 여부 확인
+--   → 프로시저 호출
+EXEC PRC_MEMBER_INSERT('소서현', '010-2222-2222', '경기');
+--==>> PL/SQL 프로시저가 성공적으로 완료되었습니다.
+
+SELECT *
+FROM TBL_MEMBER;
+--==>>
+/*
+1	정주희	010-1111-1111	서울
+2	소서현	010-2222-2222	경기
+*/
+
+
+--○ 생성한 프로시저(PRC_MEMBER_INSERT)가 제대로 동작하는지의 여부 확인
+--   → 프로시저 호출
+EXEC PRC_MEMBER_INSERT('김아별', '010-3333-3333', '인천');
+--==>> PL/SQL 프로시저가 성공적으로 완료되었습니다.
+
+SELECT *
+FROM TBL_MEMBER;
+--==>>
+/*
+1	정주희	010-1111-1111	서울
+2	소서현	010-2222-2222	경기
+3	김아별	010-3333-3333	인천
+*/
+
+--○ 생성한 프로시저(PRC_MEMBER_INSERT)가 제대로 동작하는지의 여부 확인
+--   → 프로시저 호출
+EXEC PRC_MEMBER_INSERT('이하림', '010-4444-4444', '부산');
+--==>> 에러 발생
+/*
+명령의 345 행에서 시작하는 중 오류 발생 -
+BEGIN PRC_MEMBER_INSERT('이하림', '010-4444-4444', '부산'); END;
+오류 보고 -
+ORA-20001: 서울,인천,경기만 입력 가능합니다.
+ORA-06512: at "SCOTT.PRC_MEMBER_INSERT", line 56
+ORA-06512: at line 1
+*/
+
+
+--○ 생성한 프로시저(PRC_MEMBER_INSERT)가 제대로 동작하는지의 여부 확인
+--   → 프로시저 호출
+EXEC PRC_MEMBER_INSERT('박민지', '010-5555-5555', '대전');
+--==>> 에러 발생
+/*
+ORA-20001: 서울,인천,경기만 입력 가능합니다.
+*/
+
+--○ 실습 테이블 생성(TBL_출고)
+CREATE TABLE TBL_출고
+( 출고번호  NUMBER
+, 상품코드  VARCHAR2(20)
+, 출고일자  DATE    DEFAULT SYSDATE
+, 출고수량  NUMBER
+, 출고단가  NUMBER
+);
+--==>> Table TBL_출고이(가) 생성되었습니다.
+
+--○ TBL_출고 테이블의 출고번호에 PK 제약조건 지정
+ALTER TABLE TBL_출고
+ADD CONSTRAINT 출고_출고번호_PK PRIMARY KEY(출고번호);
+--==>> Table TBL_출고이(가) 변경되었습니다.
+
+--○ TBL_출고 테이블의 상품코드는 TBL_상품 테이블의 상품코드를
+--   참조할 수 있도록 외래키(FK) 제약조건 지정
+ALTER TABLE TBL_출고
+ADD CONSTRAINT 출고_상품코드_FK FOREIGN KEY(상품코드)
+               REFERENCES TBL_상품(상품코드);
+--==>> Table TBL_출고이(가) 변경되었습니다.
+
+
+
+-- 내가 풀이한 내용
+
+--○ 생성한 프로시저(PRC_MEMBER_INSERT)가 제대로 동작하는지의 여부 확인
+--   → 프로시저 호출
+EXEC PRC_출고_INSERT('H001', 40, 1000);
+--==>> PL/SQL 프로시저가 성공적으로 완료되었습니다.
+
+--○ 생성한 프로시저(PRC_MEMBER_INSERT)가 제대로 동작하는지의 여부 확인
+--   → 프로시저 호출
+EXEC PRC_출고_INSERT('H001', 100, 1000);
+--==>> 에러발생
+/*
+명령의 394 행에서 시작하는 중 오류 발생 -
+BEGIN PRC_출고_INSERT('H001', 100, 1000); END;
+오류 보고 -
+ORA-20001: 출고수량이 재고수량보다 많습니다.
+ORA-06512: at "SCOTT.PRC_출고_INSERT", line 37
+ORA-06512: at line 1
+*/
+--> 재고수량 보다 더 많이 출고 시 에러를 발생시킴 
+
+-- 함께 풀이한 내용
+
+--○ 생성한 프로시저(PRC_MEMBER_INSERT)가 제대로 동작하는지의 여부 확인
+--   → 프로시저 호출
+EXEC PRC_출고_INSERT('H001', 10, 1000);
+--==>> PL/SQL 프로시저가 성공적으로 완료되었습니다.
+
+SELECT *
+FROM TBL_출고;
+--==>> 1	H001	2021-04-09	10	1000
+
+SELECT *
+FROM TBL_상품;
+--==>> H001	홈런볼	1500	50
+
+--○ 생성한 프로시저(PRC_MEMBER_INSERT)가 제대로 동작하는지의 여부 확인
+--   → 프로시저 호출
+EXEC PRC_출고_INSERT('H002', 10, 1000);
+--==>> 에러발생
+/*
+ORA-20002: 재고 부족~!!!
+*/
+
+
+--○ 생성한 프로시저(PRC_MEMBER_INSERT)가 제대로 동작하는지의 여부 확인
+--   → 프로시저 호출
+EXEC PRC_출고_INSERT('H001', 60, 1000);
+--==>> 에러발생
+/*
+ORA-20002: 재고 부족~!!!
+*/
+
+
+--○ 생성한 프로시저(PRC_MEMBER_INSERT)가 제대로 동작하는지의 여부 확인
+--   → 프로시저 호출
+EXEC PRC_출고_INSERT('H001', 50, 1000);
+--==>> PL/SQL 프로시저가 성공적으로 완료되었습니다.
+
+SELECT *
+FROM TBL_출고;
+--==>> 
+/*
+1	H001	2021-04-09	10	1000
+2	H001	2021-04-09	50	1000
+*/
+
+SELECT *
+FROM TBL_상품;
+--==>> H001	홈런볼	1500	0
+
+
+-- 입고/출고 내역 및 재고수량 초기화
+TRUNCATE TABLE TBL_입고;
+--==>> Table TBL_입고이(가) 잘렸습니다.
+TRUNCATE TABLE TBL_출고;
+--==>> Table TBL_출고이(가) 잘렸습니다.
+
+UPDATE TBL_상품
+SET 재고수량 = 0;
+--==>> 21개 행 이(가) 업데이트되었습니다.
+
+-- 확인
+SELECT *
+FROM TBL_입고;
+
+SELECT *
+FROM TBL_출고;
+
+SELECT *
+FROM TBL_상품;
+
+
+-- 입고 이벤트 발생
+EXEC PRC_입고_INSERT('H001', 20, 400);
+EXEC PRC_입고_INSERT('H002', 30, 500);
+EXEC PRC_입고_INSERT('H003', 40, 600);
+EXEC PRC_입고_INSERT('H004', 50, 700);
+EXEC PRC_입고_INSERT('H005', 60, 800);
+EXEC PRC_입고_INSERT('H006', 70, 900);
+EXEC PRC_입고_INSERT('H007', 80, 1000);
+--==>> PL/SQL 프로시저가 성공적으로 완료되었습니다. * 7
+
+EXEC PRC_입고_INSERT('C001', 30, 800);
+EXEC PRC_입고_INSERT('C002', 40, 900);
+EXEC PRC_입고_INSERT('C003', 50, 1000);
+EXEC PRC_입고_INSERT('C004', 60, 1100);
+EXEC PRC_입고_INSERT('C005', 70, 1200);
+EXEC PRC_입고_INSERT('C006', 80, 1300);
+EXEC PRC_입고_INSERT('C007', 90, 1400);
+--==>> PL/SQL 프로시저가 성공적으로 완료되었습니다. * 7
+
+EXEC PRC_입고_INSERT('E001', 80, 990);
+EXEC PRC_입고_INSERT('E002', 70, 880);
+EXEC PRC_입고_INSERT('E003', 60, 770);
+EXEC PRC_입고_INSERT('E004', 50, 660);
+EXEC PRC_입고_INSERT('E005', 40, 550);
+EXEC PRC_입고_INSERT('E006', 30, 440);
+EXEC PRC_입고_INSERT('E007', 20, 330);
+--==>> PL/SQL 프로시저가 성공적으로 완료되었습니다. * 7
+
+
+
+-- 출고 이벤트 발생
+EXEC PRC_출고_INSERT('H001', 1, 1100);
+EXEC PRC_출고_INSERT('H002', 2, 1200);
+EXEC PRC_출고_INSERT('H003', 3, 1300);
+EXEC PRC_출고_INSERT('H004', 4, 1400);
+EXEC PRC_출고_INSERT('H005', 5, 1500);
+EXEC PRC_출고_INSERT('H006', 6, 1600);
+EXEC PRC_출고_INSERT('H007', 7, 1700);
+--==>> PL/SQL 프로시저가 성공적으로 완료되었습니다. * 7
+
+EXEC PRC_출고_INSERT('C001', 2, 1900);
+EXEC PRC_출고_INSERT('C002', 3, 1910);
+EXEC PRC_출고_INSERT('C003', 4, 1920);
+EXEC PRC_출고_INSERT('C004', 5, 1930);
+EXEC PRC_출고_INSERT('C005', 6, 1940);
+EXEC PRC_출고_INSERT('C006', 7, 1950);
+EXEC PRC_출고_INSERT('C007', 8, 1960);
+--==>> PL/SQL 프로시저가 성공적으로 완료되었습니다. * 7
+
+SELECT *
+FROM TBL_입고;
+
+SELECT *
+FROM TBL_출고;
+--==>>
+/*
+1	H001	2021-04-09	1	1100
+2	H002	2021-04-09	2	1200
+3	H003	2021-04-09	3	1300
+4	H004	2021-04-09	4	1400
+5	H005	2021-04-09	5	1500
+6	H006	2021-04-09	6	1600
+7	H007	2021-04-09	7	1700
+8	C001	2021-04-09	2	1900
+9	C002	2021-04-09	3	1910
+10	C003	2021-04-09	4	1920
+11	C004	2021-04-09	5	1930
+12	C005	2021-04-09	6	1940
+13	C006	2021-04-09	7	1950
+14	C007	2021-04-09	8	1960
+*/
+
+SELECT *
+FROM TBL_상품;
+--==>>
+/*
+H001	홈런볼	1500	19
+H002	새우깡	1200	28
+H003	자갈치	1000	37
+H004	감자깡	 900	46
+H005	꼬깔콘	1100	55
+H006	꼬북칩	2000	64
+H007	맛동산	1700	73
+C001	다이제	2000	28
+C002	사브레	1800	37
+C003	에이스	1700	46
+C004	버터링	1900	55
+C005	아이비	1700	64
+C006	웨하스	1200	73
+C007	오레오	1900	82
+E001	엠엔엠	 600	80
+E002	아폴로	 500	70
+E003	쫀드기	 300	60
+E004	비틀즈	 600	50
+E005	마이쮸	 800	40
+E006	에그몽	 900	30
+E007	차카니	 900	20
+*/
+
+-- 내가 풀이한 내용
+
+--○ 생성한 프로시저(PRC_MEMBER_INSERT)가 제대로 동작하는지의 여부 확인
+--   → 프로시저 호출
+EXEC PRC_출고_UPDATE(14, 20);
+--==>> PL/SQL 프로시저가 성공적으로 완료되었습니다.
+
+SELECT *
+FROM TBL_출고;
+--==>> 14	C007	2021-04-09	20	1960
+
+SELECT *
+FROM TBL_상품;
+--==>> C007	오레오	1900	70
+
+--○ 생성한 프로시저(PRC_MEMBER_INSERT)가 제대로 동작하는지의 여부 확인
+--   → 프로시저 호출
+EXEC PRC_출고_UPDATE(14, 100);
+--==>> 에러 발생
+/*
+ORA-20003: 변경할 수량이 재고수량보다 많습니다.
+*/
+
+--○ 생성한 프로시저(PRC_MEMBER_INSERT)가 제대로 동작하는지의 여부 확인 후
+--   데이터를 원래대로 복구시키는 작업 수행
+--   → 프로시저 호출
+EXEC PRC_출고_UPDATE(14, 8);
+--==>> PL/SQL 프로시저가 성공적으로 완료되었습니다.
+
+SELECT *
+FROM TBL_출고;
+--==>> 14	C007	2021-04-09	8	1960
+
+SELECT *
+FROM TBL_상품;
+--==>> C007	오레오	1900	82
+
+
+
+
